34d1adee997f70a0e3f859578f85ba48c7c9f7d3
dcolchao <danielesteban.colchaoperez@everis.nttdata.com>
2022-08-15 19:40:06 -0500

    cambio de repo

--- MS/Operation/src/main/java/pe/com/nttdata/Operation/OperationApplication.java
+++ MS/Operation/src/main/java/pe/com/nttdata/Operation/OperationApplication.java
@@ -2,8 +2,10 @@ package pe.com.nttdata.Operation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OperationApplication {
 
 	public static void main(String[] args) {
